4ec73d7a386880d6ced4426ae59312257ba72c0c
Paul Maksimovich <paul.maksimovich@tylertech.com>
2025-06-05 13:07:55 -0400

    error context

--- src/api/traits.rs
+++ src/api/traits.rs
@@ -67,13 +67,16 @@ where
         let table: Table = self.table().await?;
         let ctx = SessionContext::new();
         let table_provider = Arc::new(IcebergTableProvider::try_new_from_table(table).await?);
-        ctx.register_table(self.table_reference().await?, table_provider)?;
+        ctx.register_table(
+            self.table_reference().await.context("Failed to get table reference")?, 
+            table_provider)
+            .context("Failed to register table")?;
         Ok(ctx)
     }
 
     async fn sql(&self, sql: &str) -> Result<DataFrame> {
         self.context()
-            .await?
+            .await.context("Failed to get session context")?
             .sql(sql)
             .await
             .context("Failed to execute query")
