84bb19d8e9b01ff14bd2e1134bb6bd51adef631a
Paul Maksimovich <paul.maksimovich@tylertech.com>
2025-06-10 12:58:18 -0400

    dont buffer the buffer

--- src/cli/util/ipc.rs
+++ src/cli/util/ipc.rs
@@ -2,7 +2,6 @@ use clap::Subcommand;
 use polars::io::SerReader;
 use polars::prelude::IpcStreamReader;
 use std::io;
-use std::io::Read;
 
 #[derive(Subcommand)]
 pub enum IpcCommand {
@@ -13,10 +12,7 @@ pub enum IpcCommand {
 pub async fn handle_util_ipc(ipc_command: IpcCommand) -> anyhow::Result<()> {
     match ipc_command {
         IpcCommand::Print => {
-            let mut buffer = Vec::new();
-            io::stdin().read_to_end(&mut buffer)?;
-            let cursor = io::Cursor::new(buffer);
-            let df = IpcStreamReader::new(cursor).finish()?;
+            let df = IpcStreamReader::new(io::stdin()).finish()?;
             println!("{}", df);
             Ok(())
         }
