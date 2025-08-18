8775ba64b3c6f5e4f820da561426121f90d4f5f9
Paul Maksimovich <paul.maksimovich@tylertech.com>
2025-08-18 14:48:17 -0400

    cleaup dead code

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,9 +1,8 @@
 use crate::cli::ai::handle_ai;
-use crate::cli::info::lookup::{InfoLookupCommand, handle_lookup};
-use crate::cli::info::{InfoCommand, handle_info};
-use crate::cli::serve::{ServeCommand, handle_serve};
-use crate::cli::sql::{SqlCommand, handle_sql};
-use crate::cli::util::{UtilCommand, handle_util};
+use crate::cli::info::{handle_info, InfoCommand};
+use crate::cli::serve::{handle_serve, ServeCommand};
+use crate::cli::sql::{handle_sql, SqlCommand};
+use crate::cli::util::{handle_util, UtilCommand};
 use anyhow::Result;
 use clap::{Parser, Subcommand};
 pub mod ai;
@@ -38,16 +37,5 @@ impl DiciCli {
             Commands::Serve(args) => handle_serve(args).await,
             Commands::Ai => handle_ai().await,
         }
-    }
-}
-#[derive(Parser)]
-#[command(version)]
-pub struct InfoMcpCli {
-    #[command(subcommand)]
-    pub command: InfoLookupCommand,
-}
-impl InfoMcpCli {
-    pub async fn run(self) -> Result<()> {
-        handle_lookup(self.command).await
     }
 }
